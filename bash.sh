ae6abfe5da99b4a90c097b9c3e3806f23e178a54
aashirjaved <aas.jav@gmail.com>
2018-11-08 12:18:45 +0500

    added copying of images

--- bash.sh
+++ bash.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
-
-while true; do
-read -rsn1 input
-if [ "$input" = "a" ]; then
-    echo "hello world"
-fi
+#!/bin/bash 
+yourfilenames=`ls images/*.png`
+for eachfile in $yourfilenames 
+do
+   feh $eachfile &
+   read -rsn1 input
+   if [ "$input" = "a" ]; then
+     cp $eachfile human/
+     echo "human"
+     
+ fi
+ if [ "$input" = "s" ]; then
+     cp $eachfile car/
+     echo "car"
+     
+ fi
+  if [ "$input" = "d" ]; then
+     cp $eachfile others/
+     echo "others"
+ fi
 done
